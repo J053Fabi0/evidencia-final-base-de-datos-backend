4ae72de9f3e216ab73ad0d50f33ee1a46a380510
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-04-10 19:46:56 -0600

    Updated deps with ObjectSchema

--- deps.ts
+++ deps.ts
@@ -2,6 +2,7 @@ import "dotenv";
 
 export { opineCors } from "cors";
 export { default as Joi } from "joi";
+export type { ObjectSchema } from "joi";
 
 export { default as opine, json, Router } from "opine";
 export type { NextFunction, OpineResponse, OpineRequest, Params, ParamsDictionary } from "opine";
--- schemas/opine/schemaUtils.ts
+++ schemas/opine/schemaUtils.ts
@@ -1,15 +1,7 @@
-import { Joi, NextFunction } from "../../deps.ts";
 import CommonRequest from "../../types/commonRequest.type.ts";
 import CommonResponse from "../../types/commonResponse.type.ts";
+import { Joi, NextFunction, ObjectSchema } from "../../deps.ts";
 import validateRequest, { Element } from "../../utils/validateRequest.ts";
-import { ObjectSchema } from "https://esm.sh/v114/joi@17.6.4/lib/index.js";
-
-// export const validIDs =
-//   (db: Collection<any>) =>
-//   (id: number, { error }: Joi.CustomHelpers<any>) => {
-//     const valids = db.find({}).map(({ $loki }) => $loki);
-//     return valids.includes(id) ? id : error("any.only", { valids });
-//   };
 
 export const joi = Joi.defaults((schema) => {
   switch (schema.type) {
@@ -32,17 +24,3 @@ export const a =
     validateRequest(req, res, next, schema, element);
 
 export const id = joi.string().length(24).hex();
-
-// export const optionalArrayWithAllIDsOfDB = (db: Collection<any>) =>
-//   Joi.when(Joi.ref("."), {
-//     is: Joi.array(),
-//     then: Joi.array()
-//       .min(1)
-//       .unique()
-//       .items(Joi.number().custom(validIDs(db))),
-
-//     // If it is only one value, it will be converted to number, verified and then converted to an array
-//     otherwise: Joi.number()
-//       .custom(validIDs(db))
-//       .custom((value) => [value]),
-//   });
