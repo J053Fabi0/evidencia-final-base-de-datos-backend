fc3e12ae8398ad42b5ca701fc441fd6391855d27
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-04-22 18:44:18 -0600

    Updated dependencies

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import { opine, opineCors, json, mongoose, NextFunction } from "./deps.ts";
 // Check that all required .env variables are set
 const envVariables = ["PORT", "MONGO_URI"];
 for (const envVariable of envVariables)
-  if (Deno.env.get(envVariable) === undefined) console.log(envVariable + " not set in .env."), Deno.exit(0);
+  if (Deno.env.get(envVariable) === undefined) console.log(`${envVariable} not set in .env.`), Deno.exit(0);
 
 // Connect to MongoDB
 await mongoose.connect(Deno.env.get("MONGO_URI")!);
