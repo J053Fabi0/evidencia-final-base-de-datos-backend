e16df2cec4b9747e8aee038a25589f9dc8547252
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-04-30 22:26:38 -0600

    Created "BASE_URL" env variable

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import CommonResponse from "./types/commonResponse.type.ts";
 import { opine, opineCors, json, mongoose, NextFunction } from "./deps.ts";
 
 // Check that all required .env variables are set
-const envVariables = ["PORT", "MONGO_URI"];
+const envVariables = ["PORT", "MONGO_URI", "BASE_URL"];
 for (const envVariable of envVariables)
   if (Deno.env.get(envVariable) === undefined) console.log(`${envVariable} not set in .env.`), Deno.exit(0);
 
@@ -24,7 +24,7 @@ app.use(json());
 app.use(opineCors());
 
 // Use all the routes
-app.use("/", router);
+app.use(Deno.env.get("BASE_URL") || "/", router);
 
 // Error handling
 app.use((err: Error, _: CommonRequest, res: CommonResponse, __: NextFunction) => handleError(res, err));
